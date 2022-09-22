296bc01294529d23269a4214876648974b158e78
[DMatias <[Dbazanmatias2004@gmail.com>
2022-09-22 19:23:16 -0300

    Agregando el mensaje de error en el formulario

--- js/formulario.js
+++ js/formulario.js
@@ -14,8 +14,15 @@ const validarFormulario = (e) => {
             if(expresiones.nombre.test(e.target.value)) { //e.target.value: acceder al valor en el input
                 document.getElementById('grupo__input').classList.remove('formulario__grupo-incorrecto')
                 document.getElementById('grupo__input').classList.add('formulario__grupo-correcto')
+                document.querySelector('#grupo__user i').classList.add('fa-circle-check')
+                document.querySelector('#grupo__user i').classList.remove('fa-circle-xmark')
+                document.querySelector('.formulario__grupo .formulario__parrafo-error').classList.remove('formulario__parrafo-error-activo')
             } else {
                 document.getElementById('grupo__input').classList.add('formulario__grupo-incorrecto')
+                document.getElementById('grupo__input').classList.remove('formulario__grupo-correcto')
+                document.querySelector('#grupo__user i').classList.add('fa-circle-xmark')
+                document.querySelector('#grupo__user i').classList.remove('fa-circle-check')
+                document.querySelector('.formulario__grupo .formulario__parrafo-error').classList.add('formulario__parrafo-error-activo')
             }
         break
         case 'apellido':
